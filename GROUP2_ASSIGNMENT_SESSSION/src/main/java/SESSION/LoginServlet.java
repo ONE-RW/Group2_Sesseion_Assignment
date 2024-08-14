58d60fb08da8a222a30b5b0e282a2b67b972e5c0
ONE <ONE@NIYANGE>
2024-08-14 15:52:53 +0200

    COMMIT

---
+++ GROUP2_ASSIGNMENT_SESSSION/src/main/java/SESSION/LoginServlet.java
@@ -0,0 +1,32 @@
+package SESSION;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "admin";
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            HttpSession session = request.getSession(true); 
+            session.setAttribute("username", username);
+            response.sendRedirect("welcome");
+        } else {
+            response.sendRedirect("error.html");
+        }
+    }
+}
+
